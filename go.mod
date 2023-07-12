df323c296abadd4b06507f67e6fd2536f81b0fa7
Peyton Schlafley <pschlafley0@gmail.com>
2023-07-11 23:07:20 -0400

    added the create directory function to the list of options, added some green text when a file/directory was found or created. Still working on an error in the find file if block where it is automatically prompting the user to create the directory even if the directory exists

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/pschlafley/go_terminal
 go 1.20
 
 require (
-	github.com/pschlafley/fileFunctions v0.0.1-alpha.0.20230707012826-70a6b767bd47
+	github.com/pschlafley/fileFunctions v0.0.1-alpha.0.20230712022616-614a938c794f
 	github.com/pterm/pterm v0.12.62
 )
 
--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 		"Edit a file",
 		"Delete a file",
 		"Find a file",
+		"Create a Directory",
 	}
 
 	result, _ := pterm.DefaultInteractiveSelect.WithOptions(selectOptions).Show()
@@ -35,15 +36,39 @@ func main() {
 	if result == "Create a file" {
 		fileName, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("File Name").Show()
 		path, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Path").Show()
+		fileNameText := pterm.DefaultBasicText.WithStyle(pterm.FgLightGreen.ToStyle()).Sprint(fileName)
+		pathText := pterm.DefaultBasicText.WithStyle(pterm.FgLightGreen.ToStyle()).Sprint(path)
 
-		area.Update(fileName, path)
+		area.Update(pathText, fileNameText)
 		fileFunctions.CreateFile(fileName, path)
+
 	} else if result == "Find a file" {
 		fileName, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("File Name").Show()
 		path, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Path").Show()
+		fileNameText := pterm.DefaultBasicText.WithStyle(pterm.FgLightGreen.ToStyle()).Sprint(fileName)
+		pathText := pterm.DefaultBasicText.WithStyle(pterm.FgLightGreen.ToStyle()).Sprint(path)
+		confirmCreateDirectory, confirmCreateDirectoryErr := pterm.DefaultInteractiveConfirm.WithConfirmText("Yes").WithDefaultText("Would you like to create the directory and file at the given path?").WithDefaultValue(true).Show()
 
-		area.Update(fileName, path)
-		fileFunctions.FindFile(fileName, path)
+		area.Update(pathText, fileNameText)
+		fileWasFound, fileName, path, errorArr := fileFunctions.FindFile(fileName, path)
+
+		if fileWasFound {
+			area.Update(pterm.DefaultBasicText.WithStyle(pterm.FgLightGreen.ToStyle()).Sprintf("%v was found at %v", fileName, path))
+		} else if errorArr[0] == "no such file or directory" {
+			area.Update(confirmCreateDirectory)
+
+			if confirmCreateDirectory {
+				fileFunctions.CreateDirectory(path)
+				fileFunctions.CreateFile(fileName, path)
+			} else if !confirmCreateDirectory {
+				area.Update(pterm.DefaultBasicText.WithStyle(pterm.FgRed.ToStyle()).Print(confirmCreateDirectoryErr))
+			}
+		}
+
+	} else if result == "Create a Directory" {
+		path, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Path").Show()
+
+		fileFunctions.CreateDirectory(path)
 	}
 
 	area.Stop()
